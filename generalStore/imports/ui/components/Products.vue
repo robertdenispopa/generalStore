3fcf8e33ed421d00e53d02aaa3730232f4c7087b
OttoR3V <ottimuhlroth@yahoo.com>
2022-05-23 00:29:14 +0200

    GEN-9 ## Import library

---
+++ generalStore/imports/ui/components/Products.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="app">
+        
+        <div class="main">
+            <div class ="title-page">
+                <h2>Products</h2>
+            </div>
+            <div v-if="this.currentUser.profile.usertype === 'Seller' ">
+                <ProductForm/>
+            </div>
+            <table class="products">
+                <tr>
+                    <th> Name </th>
+                    <th> Description </th>
+                    <th> Price </th>
+                    <th></th>
+                </tr>
+                <Product 
+                class="product"
+                v-for="product in products"
+                v-bind:key="product._id"
+                v-bind:product="product" 
+                />
+                
+            </table>
+            <ul >
+                
+            </ul>
+        </div>
+    </div>
+</template>
+
+import Vue from "vue";
+import Product from "./Product.vue";
+import ProductForm from "./ProductForm.vue";
+import { ProductsCollection } from "../../api/collections/ProductsCollection.js";
