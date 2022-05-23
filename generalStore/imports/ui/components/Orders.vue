1f71cce3e701744d87401794c8865095e03e3893
OttoR3V <ottimuhlroth@yahoo.com>
2022-05-23 18:57:34 +0200

    GEN-16 ## UI for Orders

---
+++ generalStore/imports/ui/components/Orders.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="app">
+        <div class="main">
+            <div class ="title-page">
+                <h2>Orders
+                <span v-if="incompleteCount > 0">({{incompleteCount}})</span>
+                </h2>
+            </div>
+            <div class="filter">
+                <button
+                    v-model="hideCompleted"
+                    @click="toggleHideCompleted"
+                    >
+                <span v-if="hideCompleted">Show All</span>
+                <span v-else>Hide Completed Orders</span>
+                </button>
+            </div>
+            <div class="loading" v-if="!$subReady.orders">Loading...</div>
+            <table class="products">
+                <tr>
+                    <th> Name </th>
+                    <th> Description </th>
+                    <th> Price </th>
+                    <th> Address </th>
+                    <th> Completed </th>
+                </tr>
+                <Order 
+                class="product"
+                v-for="order in orders"
+                v-bind:key="order._id"
+                v-bind:order="order" 
+                />
+                
+            </table>
+        </div>
+    </div>
+</template>
