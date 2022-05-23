d07c1c36d09c0fdb3550e011cbcefe2bb40877b4
OttoR3V <ottimuhlroth@yahoo.com>
2022-05-23 08:51:18 +0200

    UI for orders

---
+++ generalStore/imports/ui/components/Order.vue
@@ -0,0 +1,14 @@
+<template>
+        <tr v-bind:class="orderClassName">
+            <td>{{ this.order.name}}</td>
+            <td>{{ this.order.description}}</td>
+            <td>{{ this.order.price}}</td>
+            <td>{{ this.order.address}}</td>
+            <td> <input
+            type="checkbox"
+            readOnly
+            v-bind:checked="!!this.order.checked"
+            @click="toggleChecked"
+            /> </td>
+        </tr>   
+</template>
