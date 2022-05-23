caa9dfdab963987eca72b5a790bd6736d98875a8
OttoR3V <ottimuhlroth@yahoo.com>
2022-05-23 08:56:30 +0200

    GEN-19 ## Some code added for functionality(it still doesnt work)

--- generalStore/imports/ui/components/Order.vue
+++ generalStore/imports/ui/components/Order.vue
@@ -12,3 +12,24 @@
             /> </td>
         </tr>   
 </template>
+
+<script>
+    import Vue from "vue";
+    import { OrdersCollection } from "../../api/collections/OrdersCollection.js";
+    export default {
+    props: ["order"],
+        data() {
+        return {};
+        },
+    computed: {
+        orderClassName: function() {
+        return this.order.checked ? "checked" : "";
+        }
+    },
+    methods: {
+        toggleChecked() {
+        Meteor.call('orders.setIsChecked', this.order._id, !this.order.checked);
+        },
+    }
+};
+</script>
