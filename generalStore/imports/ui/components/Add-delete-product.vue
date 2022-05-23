2c814351cba0702b51c27ea2d784649b81c53ef8
OttoR3V <ottimuhlroth@yahoo.com>
2022-05-23 09:24:57 +0200

    GEN-22 ## Adding two buttons for delete and add

---
+++ generalStore/imports/ui/components/Add-delete-product.vue
@@ -0,0 +1,10 @@
+<template>
+        <tr>
+<td v-if="currentUser.profile.usertype === 'Seller'">
+<button class="delete" @click="deleteThisProduct">×</button>
+            </td>
+            <td v-if="currentUser.profile.usertype === 'Buyer'">
+                <button class="add" @click="addThisProduct" >Add</button>
+            </td>
+        </tr>   
+</template>
