1ff3ca94c3a466c0f2697a5e94f9b298da3472a6
OttoR3V <ottimuhlroth@yahoo.com>
2022-05-23 10:23:59 +0200

    GEN-22 ## Added some functionality to buttons

--- generalStore/imports/ui/components/Add-delete-product.vue
+++ generalStore/imports/ui/components/Add-delete-product.vue
@@ -1,10 +1,43 @@
 <template>
         <tr>
-<td v-if="currentUser.profile.usertype === 'Seller'">
-<button class="delete" @click="deleteThisProduct">×</button>
+            <td>{{ this.product.name}}</td>
+            <td>{{ this.product.description}}</td>
+            <td>{{ this.product.price}}</td>
+        <td v-if="currentUser.profile.usertype === 'Seller'">
+        <button class="delete" @click="deleteThisProduct">×</button>
             </td>
             <td v-if="currentUser.profile.usertype === 'Buyer'">
                 <button class="add" @click="addThisProduct" >Add</button>
             </td>
         </tr>   
 </template>
+
+<script>
+    import Vue from "vue";
+    export default {
+    props: ["product"],
+        data() {
+        return {};
+        },
+    methods: {
+        deleteThisProduct() {
+        Meteor.call('products.remove', this.product._id);
+    },
+        addThisProduct() {
+            var toSend = {
+                    name:this.product.name,
+                    description:this.product.description,
+                    price:this.product.price,
+                    userOfItem:this.product.userId
+                };
+        
+            Meteor.call('cart.insert', toSend);
+        },
+    },
+    meteor:{
+        currentUser() {
+            return Meteor.user(); 
+            }
+    }
+};
+</script>
